685cb8a972b10480f179e6adf974cf74c61e311e
Jeremy <jpenakuralabs@outlook.com>
2024-08-13 23:01:02 +0000

    adding binbash

--- backupdirectory.sh
+++ backupdirectory.sh
@@ -1,10 +1,18 @@
+#!/bin/bash 
 #check if backup dir exist
 #if it does do nothing
 #if it doesn't create it
 
 
-mkdir -p backup
+#mkdir -p backup
 
-#dirname=backup
+bdir=backup
 
-#if [[ ! -d $dirname ]] 
+if [[ ! -d "$bdir" ]]
+then
+    echo "File does NOT exist. Generating"
+    mkdir ./$bdir
+    echo "backup directory created"
+else
+    echo "File exists"
+fi 
